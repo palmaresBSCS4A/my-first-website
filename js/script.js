fb360999228052247fe7562fef4543a3242547ef
Mary Angel Palmares <160818948+palmaresBSCS4A@users.noreply.github.com>
2025-08-31 12:20:45 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+  // Hamburger toggle
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("nav-active");
+    hamburger.classList.toggle("toggle");
+  });
